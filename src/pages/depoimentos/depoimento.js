05e219028199d2cb0a2bb3b559c09c28b7b5d0f7
Ester Alves <euesteralves@gmail.com>
2024-06-03 17:25:19 -0300

    depoimentos - alterações

--- src/pages/depoimentos/depoimento.js
+++ src/pages/depoimentos/depoimento.js
@@ -1,54 +1,63 @@
-// FORM 
+// FORM
 function render(data){
-    var html = "<div class='comentBox'><div class='leftPanelImg'><img src='../../../src/assets/icons/usuario-de-perfil.png'></div><h1>"+data.name+"</h1><p>"+data.body+"</p></div><div class='clear'></div></div>"
-    $('#containercomment').append(html);
+  var html = "<div class='comentBox'><div class='leftPanelImg'><img src='img/usuario-de-perfil.png'></div><h1>"+data.name+"</h1><p>"+data.body+"</p></div><div class='clear'></div></div>"
+  $('#containercomment').append(html);
+}
+$(document).ready(function(){
+
+  var coment = [];
+
+  if(!localStorage.comentData){
+    localStorage.comentData = [];
   }
-  $(document).ready(function(){
+  else{
+    coment = JSON.parse(localStorage.comentData);
+  }
   
-    var coment = [];
+  for (var i = 0; i < coment.length; i++){
+    render(coment[i]);
+  }
   
-    if(!localStorage.comentData){
-      localStorage.comentData = [];
+  $('#addComent').click(function(e){
+    e.preventDefault();
+
+    if ($('#bodyText').val().trim() !== '') {
+      if (($('#name').val().trim() !== '') || ($('#agreeTerms').prop('checked'))) {
+        var name = $('#agreeTerms').prop('checked') ? "Anônimo" : $('#name').val();
+        var addObj = {
+            "name": name,
+            "body": $('#bodyText').val()
+        };
+
+        coment.push(addObj);
+        localStorage.comentData = JSON.stringify(coment);
+        render(addObj);
+        $('#name').val('');
+        $('#bodyText').val('');
+        $('#agreeTerms').prop('checked', false);
+        
+        openP(); 
+      } else {
+        alert('Por favor, escolha entre fornecer o seu nome ou selecionar Anônimo.'); // Exibe uma mensagem de alerta
+      }
+    } else {
+        alert('Por favor, insira um depoimento antes de enviar.'); // Exibe uma mensagem de alerta
     }
-    else{
-      coment = JSON.parse(localStorage.comentData);
-    }
-    
-    for (var i = 0; i < coment.length; i++){
-      render(coment[i]);
-    }
-    
-    $('#addComent').click(function(){
-      var addObj = {
-        "name": $('#name').val(),
-        "body": $('#bodyText').val(),
-      };
-  
-      coment.push(addObj);
-      localStorage.comentData =JSON.stringify(coment);
-      render(addObj);
-      $('#name').val('');
-  
-      $('#bodyText').val('');
-    });
-  
   });
-  
-  
-  
-  // POPUP
-  
-  const popupdep = document.querySelector('#pop');
-  const overlay = document.querySelector('.overlay');
-  
-  function openP() {
-      popupdep.classList.add('open');
-      overlay.classList.add('open');
-  }
-  
-  function closeP() {
-      popupdep.classList.remove('open');
-      overlay.classList.remove('open');
-  }
-  
-  
+});
+
+
+// POPUP
+
+const popupdep = document.querySelector('#pop');
+const overlay = document.querySelector('.overlay');
+
+function openP() {
+    popupdep.classList.add('open');
+    overlay.classList.add('open');
+}
+
+function closeP() {
+    popupdep.classList.remove('open');
+    overlay.classList.remove('open');
+}
