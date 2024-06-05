97250f14992debd356f4022090828d77c44e0604
Ester Alves <euesteralves@gmail.com>
2024-06-05 11:36:13 -0300

    pag depoimentos - alterações finais - js

--- src/pages/depoimentos/depoimento.js
+++ src/pages/depoimentos/depoimento.js
@@ -1,6 +1,6 @@
 // FORM
 function render(data){
-  var html = "<div class='comentBox'><div class='leftPanelImg'><img src='img/usuario-de-perfil.png'></div><h1>"+data.name+"</h1><p>"+data.body+"</p></div><div class='clear'></div></div>"
+  var html = "<div class='comentBox'><div class='leftPanelImg'><img src='../../assets/icons/usuario-de-perfil.png'></div><h1>"+data.name+"</h1><p>"+data.body+"</p></div><div class='clear'></div></div>"
   $('#containercomment').append(html);
 }
 $(document).ready(function(){
@@ -61,3 +61,4 @@ function closeP() {
     popupdep.classList.remove('open');
     overlay.classList.remove('open');
 }
+
